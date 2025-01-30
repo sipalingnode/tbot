f0bac03c9823baceaa723167f6dbd142fb691b60
Node Army <109174478+sipalingnode@users.noreply.github.com>
2025-01-30 20:30:21 +0700

    Create network_config.py

---
+++ network_config.py
@@ -0,0 +1,24 @@
+# network_config.py
+
+networks = {
+    'Arbitrum Sepolia': {
+        'rpc_url': 'https://arb-sepolia.g.alchemy.com/v2/xYu4WpjXgnrRuWucBGGiWzXeott18G-9',
+        'chain_id': 421614,
+        'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
+    },
+    'OP Sepolia': {
+        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/Xr-VU76P_nl32pDDXh102_x6zRQsxxaV',
+        'chain_id': 11155420,
+        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
+    },
+    'Blast Sepolia': {
+        'rpc_url': 'https://blast-sepolia.g.alchemy.com/v2/Gv98S5D61xGly9QOcYhtmVMKOM5g06rT',
+        'chain_id': 168587773,
+        'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2'
+    },
+    'Base Sepolia': {
+        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/ia2mGIJ9XRc2xrsJOsyrjU5QkoVqCvtu',
+        'chain_id': 84532,
+        'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'
+    }
+}
