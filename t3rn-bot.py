bf31c37cc5e2d6b68dfe9036f42f6860ecdf63ef
hanifdamarjati20 <hanif.damarjati20@gmail.com>
2025-02-02 21:50:47 +0700

    fix

--- t3rn-bot.py
+++ t3rn-bot.py
@@ -10,11 +10,11 @@ def center_text(text):
     terminal_width = os.get_terminal_size().columns
     lines = text.splitlines()
     centered_lines = [line.center(terminal_width) for line in lines]
-return "\n".join(centered_lines)
+    return "\n".join(centered_lines)
 
 # Fungsi untuk membersihkan terminal
 def clear_terminal():
-os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 ascii_art = """
 
@@ -50,7 +50,7 @@ explorer_urls = {
 
 def get_brn_balance(web3, my_address):
     balance = web3.eth.get_balance(my_address)
-return web3.from_wei(balance, 'ether')
+    return web3.from_wei(balance, 'ether')
 
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     my_address = Web3.to_checksum_address(my_address)  # Mengonversi alamat ke format checksum
@@ -68,7 +68,7 @@ def send_bridge_transaction(web3, account, my_address, data, network_name):
         gas_limit = gas_estimate + 50000
     except Exception as e:
         print(f"Error estimating gas: {e}")
-        return None
+    return None
 
     base_fee = web3.eth.get_block('latest')['baseFeePerGas']
     priority_fee = web3.to_wei(5, 'gwei')
@@ -112,12 +112,12 @@ def send_bridge_transaction(web3, account, my_address, data, network_name):
         return web3.to_hex(tx_hash), value_in_ether
     except Exception as e:
         print(f"Error sending transaction: {e}")
-return None, None
+    return None, None
 
 def process_network_transactions(network_name, bridges, chain_data, successful_txs):
     web3 = Web3(Web3.HTTPProvider(chain_data['rpc_url']))
     if not web3.is_connected():
-    raise Exception(f"Tidak dapat terhubung ke jaringan {network_name}")
+        raise Exception(f"Tidak dapat terhubung ke jaringan {network_name}")
 
     for bridge in bridges:
         for i, private_key in enumerate(private_keys):
@@ -131,10 +131,10 @@ def process_network_transactions(network_name, bridges, chain_data, successful_t
                     print(f"{chain_symbols[network_name]} Total Tx Sukses: {successful_txs} | {labels[i]} | Bridge: {bridge} | Jumlah Bridge: {value_sent:.5f} ETH ✅{reset_color}\n")
                 else:
                     print(f"{chain_symbols[network_name]} Total Tx Sukses: {successful_txs} | {labels[i]} | Bridge: {bridge} ✅{reset_color}\n")
-            print(f"{'='*150}")
-        print("\n")
-    time.sleep(3)
-return successful_txs
+                    print(f"{'='*150}")
+                    print("\n")
+                time.sleep(3)
+    return successful_txs
 
 def display_menu():
     print(f"{menu_color}Pilih chain untuk menjalankan transaksi:{reset_color}")
@@ -146,7 +146,7 @@ def display_menu():
     print(f"{menu_color}5. Run all transactions repeatedly..recommended.. {reset_color}")
     print("")
     choice = input("choose (1-5): ")
-return choice
+    return choice
 
 def main():
     print("\033[92m" + center_text(ascii_art) + "\033[0m")   
@@ -206,7 +206,7 @@ def main():
     except Exception as e:
         print(f"Terjadi kesalahan: {e}")
         print("Wait 5 Second for Safety...")
-time.sleep(5)
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
