462ca652c84404c7bba71f7199278cb9f5b0d490
Node Army <109174478+sipalingnode@users.noreply.github.com>
2025-02-03 19:55:52 +0700

    Update t3rn-bot.py

--- t3rn-bot.py
+++ t3rn-bot.py
@@ -61,7 +61,7 @@ def get_brn_balance(web3, my_address):
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     my_address = Web3.to_checksum_address(my_address)  # Mengonversi alamat ke format checksum
     nonce = web3.eth.get_transaction_count(my_address, 'pending')
-    value_in_ether = 0.8
+    value_in_ether = 1.6
     value_in_wei = web3.to_wei(value_in_ether, 'ether')
 
     try:
