c6b1564e9790c7656e863314caac65f2bbbd6736
Node Army <109174478+sipalingnode@users.noreply.github.com>
2025-02-07 14:36:15 +0700

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,22 +2,22 @@
 
 networks = {
     'Arbitrum Sepolia': {
-        'rpc_url': 'https://arb-sepolia.g.alchemy.com/v2/xYu4WpjXgnrRuWucBGGiWzXeott18G-9',
+        'rpc_url': 'https://arbitrum-sepolia-rpc.publicnode.com',
         'chain_id': 421614,
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
     'OP Sepolia': {
-        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/Xr-VU76P_nl32pDDXh102_x6zRQsxxaV',
+        'rpc_url': 'https://sepolia.optimism.io',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
     'Blast Sepolia': {
-        'rpc_url': 'https://blast-sepolia.g.alchemy.com/v2/Gv98S5D61xGly9QOcYhtmVMKOM5g06rT',
+        'rpc_url': 'https://sepolia.blast.io',
         'chain_id': 168587773,
         'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2'
     },
     'Base Sepolia': {
-        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/ia2mGIJ9XRc2xrsJOsyrjU5QkoVqCvtu',
+        'rpc_url': 'https://base-sepolia-rpc.publicnode.com',
         'chain_id': 84532,
         'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'
     }
